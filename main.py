ac422e472d5081b7ea2ed5521fd34ad2ce4e4820
wangtianci <wangtianci@starcross.tech>
2023-07-25 13:54:05 +0800

    dev

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from src.webhooks.config import app_config
 from src.webhooks.pkg import app_pkg
 from src.webhooks.pypi import app_pypi
 from src.config.internal_config import InternalConfig
+import concurrent.futures
 
 if __name__ == '__main__':
 	config = InternalConfig()
-	app_config.run(host=f"{config.client_info.config.host}", port=int(f"{config.client_info.config.port}"))
-	app_pkg.run(host=f"{config.client_info.repo.host}", port=int(f"{config.client_info.repo.port}"))
-	app_pypi.run(host=f"{config.client_info.webhooks.host}", port=int(f"{config.client_info.webhooks.port}"))
+	with concurrent.futures.ThreadPoolExecutor(max_workers=3) as pool:
+		pool.submit(app_config.run, host=config.client_info.config.host, port=int(config.client_info.config.port))
+		pool.submit(app_pkg.run, host=config.client_info.repo.host, port=int(config.client_info.repo.port))
+		pool.submit(app_pypi.run, host=config.client_info.webhooks.host, port=int(config.client_info.webhooks.port))
