e557f5516c43519dfa702fd85d9d46eb24098549
wangtianci <wangtianci@starcross.tech>
2023-07-25 15:46:14 +0800

    dev

--- src/webhooks/pypi.py
+++ src/webhooks/pypi.py
@@ -7,7 +7,7 @@ app_pypi = Flask(__name__)
 config = InternalConfig()
 
 
-@app_pypi.route(f"/{config.client_info.base_url}/{config.client_info.webhooks.url_suffix}", methods=['POST'])
+@app_pypi.route(f"/{config.client_info.base_url}/{config.client_info.webhooks.url_suffix}", methods=['POST','GET'])
 def pypi():
 	webhooks_handler_result = event_type(request.data)
 	return webhooks_handler_result, 200
