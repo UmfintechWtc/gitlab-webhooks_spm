135d5fee628a1c24097ddf7f85e547a9160b14c8
wangtianci <wangtianci@starcross.tech>
2023-07-26 15:15:42 +0800

    dev

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
 	config = InternalConfig()
 	create_dir(config.client_info.module.pipeline_save)
 	create_dir(config.client_info.module.package_path)
-	RepoInit().update_index()
+	# RepoInit().update_index()
 	with concurrent.futures.ThreadPoolExecutor(max_workers=3) as pool:
 		pool.submit(app_config.run, host=config.client_info.config.host, port=int(config.client_info.config.port))
 		pool.submit(app_pkg.run, host=config.client_info.repo.host, port=int(config.client_info.repo.port))
--- src/webhooks/pkg.py
+++ src/webhooks/pkg.py
@@ -1,12 +1,58 @@
-from flask import Flask
-from src.cli.repo import *
-from src.config.internal_config import InternalConfig
+# app.py
+
+import os
+from flask import Flask, request, send_file
 
 app_pkg = Flask(__name__)
 
-config = InternalConfig()
+# Directory where you want to store uploaded packages
+UPLOAD_DIRECTORY = "/mnt/pypi"
 
+@app_pkg.route("/simple/<package_name>/")
+def simple_index(package_name):
+    # Replace this with your own logic to fetch package information from the repository
+    return f"""
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>{package_name}</title>
+    </head>
+    <body>
+        <h1>{package_name}</h1>
+    </body>
+    </html>
+    """
 
-@app_pkg.route("/mnt/pypi", methods=['GET'])
-def pkg():
-	return "pkg", 200
+@app_pkg.route("/simple/<package_name>/<version>/")
+def simple_detail(package_name, version):
+    # Replace this with your own logic to fetch package information from the repository
+    return f"""
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>{package_name} {version}</title>
+    </head>
+    <body>
+        <h1>{package_name} {version}</h1>
+    </body>
+    </html>
+    """
+
+@app_pkg.route("/simple/")
+def simple():
+    # Replace this with your own logic to list available packages from the repository
+    return "List of available packages"
+
+@app_pkg.route("/simple/upload/", methods=["POST"])
+def upload_package():
+    # Handle package upload and save it to the upload directory
+    package = request.files.get("package")
+    if package:
+        package.save(os.path.join(UPLOAD_DIRECTORY, package.filename))
+        return "Package uploaded successfully."
+    return "Package upload failed.", 400
+
+@app_pkg.route("/<path:filename>")
+def download_package(filename):
+    # Serve package files for download
+    return send_file(os.path.join(UPLOAD_DIRECTORY, filename), as_attachment=True)
